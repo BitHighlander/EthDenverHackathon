fec6f1f4afe64d4b3960bf7eab23a638aba7d0fd
battlejoose <battlejoose@gmail.com>
2022-02-19 20:32:44 -0700

    bufficorn

---
+++ EthDenver2022/Assets/Scripts/MyUserInfo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyUserInfo : MonoBehaviour
+{
+    public string address;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetAddress(string newAddress)
+    {
+        this.address = newAddress;
+    }
+}
